de1fefd55676971af2751614dab09e8fcfdc3c93
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2020-11-14 02:27:30 +0100

    final

--- car.js
+++ car.js
@@ -4,13 +4,12 @@ var elt = document.getElementsByTagName('button');
 addEventListener("load", function () {
 
     for (var i = 0; i < elt.length; i++) {
-
     elt[i].addEventListener("click", function() {
         if (detail === 0) {
-            document.getElementById('1st').style.display = "flex";
+            document.getElementById('0').style.display = "flex";    
             detail = 1;
         } else {
-            document.getElementById('1st').style.display = "none";
+            document.getElementById('0').style.display = "none";
             detail = 0;
         }
     })
