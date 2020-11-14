550af57f05480627dd3e23b78fae910175b8d483
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2020-11-14 02:37:01 +0100

    PROBLEM SOLVED

--- car.js
+++ car.js
@@ -1,19 +1,48 @@
-var detail = 0;
-var elt = document.getElementsByTagName('button');
-
+var detail = [0,0,0,0,0];
 addEventListener("load", function () {
-
-    for (var i = 0; i < elt.length; i++) {
-    elt[i].addEventListener("click", function() {
-        if (detail === 0) {
+    document.getElementById('dis0').addEventListener("click", function() {
+        if (detail[0] === 0) {
             document.getElementById('0').style.display = "flex";    
-            detail = 1;
+            detail[0] = 1;
         } else {
             document.getElementById('0').style.display = "none";
-            detail = 0;
+            detail[0] = 0;
         }
-    })
-
-    }
-    
+    });
+    document.getElementById('dis1').addEventListener("click", function () {
+        if (detail[1] === 0) {
+            document.getElementById('1').style.display = "flex";
+            detail[1]= 1;
+        } else {
+            document.getElementById('1').style.display = "none";
+            detail[1]= 0;
+        }
+    });
+    document.getElementById('dis2').addEventListener("click", function () {
+        if (detail[2] === 0) {
+            document.getElementById('2').style.display = "flex";
+            detail[2] = 1;
+        } else {
+            document.getElementById('2').style.display = "none";
+            detail[2] = 0;
+        }
+    });
+    document.getElementById('dis3').addEventListener("click", function () {
+        if (detail[3] === 0) {
+            document.getElementById('3').style.display = "flex";
+            detail[3] = 1;
+        } else {
+            document.getElementById('3').style.display = "none";
+            detail[3] = 0;
+        }
+    });
+    document.getElementById('dis4').addEventListener("click", function () {
+        if (detail[4] === 0) {
+            document.getElementById('4').style.display = "flex";
+            detail[4] = 1;
+        } else {
+            document.getElementById('4').style.display = "none";
+            detail[4]= 0;
+        }
+    });
 });
